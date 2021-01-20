1a770e6314f7741d8574e60af35e077e79247b34
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-01-20 15:00:43 +0100

    :construction: enabled reflection gRPC and using evans cli to access the gRPC server

--- src/main/java/org/github/felipegutierrez/explore/grpc/calculator/server/CalculatorServer.java
+++ src/main/java/org/github/felipegutierrez/explore/grpc/calculator/server/CalculatorServer.java
@@ -2,6 +2,7 @@ package org.github.felipegutierrez.explore.grpc.calculator.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.IOException;
 
@@ -11,12 +12,26 @@ public class CalculatorServer {
         server.run();
     }
 
-    public void run() {
+    private void reflectionInstructions() {
+        System.out.println("enabling reflection (use https://github.com/ktr0731/evans on the terminal:");
+        System.out.println("$ ./evans -r -p 50051)");
+        System.out.println("> show package");
+        System.out.println("> package grpc.reflection.v1alpha");
+        System.out.println("> show service");
+        System.out.println("> service CalculatorService");
+        System.out.println("> show message");
+        System.out.println("> call SquareRoot");
+        System.out.println("> ");
+    }
+
+    private void run() {
         try {
             System.out.println("Calculator server - Hello gRPC");
             Server server = ServerBuilder.forPort(50051)
                     .addService(new CalculatorServiceImpl())
+                    .addService(ProtoReflectionService.newInstance()) // enabling reflection
                     .build();
+            reflectionInstructions();
 
             server.start();
 
